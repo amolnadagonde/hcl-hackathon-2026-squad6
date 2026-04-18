1acf3c1d68b48db6ff4ba77ebe85280ac13383d1
Ikramullah  Dehlvi <ikramullah.dehlvi@mail.com>
2026-04-18 14:05:54 +0530

    Select Credit Card Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import "./App.css";
-import Login from "./pages/Login";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <>
-      <Login />
+      <Home />
     </>
   );
 }
