d05be2eb59d8e4a2fa63fa0eb489fbc4b0d4310f
amolnadagonde <amol.nadagonde@gmail.com>
2026-04-18 15:13:57 +0530

    fix

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,5 +1,6 @@
 import { useState, createContext } from "react";
 
+// eslint-disable-next-line react-refresh/only-export-components
 export const AppContext = createContext();
 
 export const AppProvider = ({ children }) => {
