ec12abb4ef36761f6712ae95849a132bc91c57be
Ikramullah  Dehlvi <ikramullah.dehlvi@mail.com>
2026-04-18 14:35:27 +0530

    Status Message Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Home from "./pages/Home";
+// import StatusPage from "./pages/StatusPage";
 
 function App() {
   return (
     <>
       <Home />
+      {/* <StatusPage /> */}
     </>
   );
 }
